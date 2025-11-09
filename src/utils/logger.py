2a38ec66a117d88d5c03dbe72a04c3478815246c
alexandarFrost <alexanderfrost8783@iejesusmirey.com>
2025-11-09 06:25:02 -0600

    feat: add src/utils/logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,12 @@
+thonimport logging
+import sys
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter("[%(levelname)s] %(asctime)s - %(name)s: %(message)s", "%H:%M:%S")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
